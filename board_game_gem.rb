c8cf1c889489068d0e53ecce934d5ef15b01391a
robertkroon <robert.kroon@ixly.nl>
2026-05-03 16:10:15 +0200

    moved games to use more bbg info

--- board_game_gem.rb
+++ board_game_gem.rb
@@ -1,91 +1,102 @@
+# frozen_string_literal: true
+
 require 'nokogiri'
-require 'open-uri'
 
 module BoardGameGem
   class BoardGameGem < BggBase
-    MAX_ATTEMPTS = 10
+    BGG_API_BASE = 'https://boardgamegeek.com/xmlapi2'
+    REQUEST_TIMEOUT = 10
+    # BGG API requires authentication - this token is from BGG developer account
+    BGG_API_TOKEN = '5c65b3d4-058e-4a1a-92e8-109d3e7658ac'
+
+    class BggApiError < StandardError; end
 
     def self.get_item(id, statistics = false, options = {})
       options[:id] = id
       options[:stats] = statistics ? 1 : 0
-      item = BggItem.new(self.request_xml("thing", options))
-      item
+      BggItem.new(request_xml('thing', options))
     end
 
     def self.get_items(ids, statistics = false, options = {})
-      options[:id] = ids.join(",")
+      options[:id] = ids.join(',')
       options[:stats] = statistics ? 1 : 0
-      item_list = []
-      path = "thing"
-      element = "item"
 
-      item_xml = BoardGameGem.request_xml(path, options)
-      item_xml.css(element).wrap("<item_data></item_data>")
-      item_xml.css("item_data").each do |item_data|
-        item_list.push(BggItem.new(item_data))
-      end
-
-      item_list
+      item_xml = request_xml('thing', options)
+      item_xml.css('item').map { |item_data| BggItem.new(item_data) }
     end
 
     def self.get_family(id, options = {})
       options[:id] = id
-      family = BggFamily.new(BoardGameGem.request_xml("family", options))
-      family.id == 0 ? nil : family
+      family = BggFamily.new(request_xml('family', options))
+      family.id.zero? ? nil : family
     end
 
     def self.get_user(username, options = {})
       options[:name] = username
-      user = BggUser.new(BoardGameGem.request_xml("user", options))
-      user.id == 0 ? nil : user
+      user = BggUser.new(request_xml('user', options))
+      user.id.zero? ? nil : user
     end
 
     def self.get_collection(username, options = {})
       options[:username] = username
-      collection_xml = BoardGameGem.request_xml("collection", options)
-      if collection_xml.css("error").length > 0
-        nil
-      else
-        BggCollection.new(collection_xml)
-      end
+      collection_xml = request_xml('collection', options)
+      collection_xml.css('error').any? ? nil : BggCollection.new(collection_xml)
     end
 
     def self.search(query, options = {})
       options[:query] = query
-      xml = BoardGameGem.request_xml("search", options)
+      xml = request_xml('search', options)
+      items_node = xml.at_css('items')
       {
-        :total => xml.at_css("items")["total"].to_i,
-        :items => xml.css("item").map { |x| BggSearchResult.new(x) }
+        total: items_node ? items_node['total'].to_i : 0,
+        items: xml.css('item').map { |x| BggSearchResult.new(x) }
       }
     end
 
-    private
+    class << self
+      private
 
-    def self.request_xml(method, hash)
-      params = BoardGameGem.hash_to_uri(hash)
-      url = "https://boardgamegeek.com/xmlapi2/#{method}?#{params}"
-
-      token = '5c65b3d4-058e-4a1a-92e8-109d3e7658ac'
-      response = RestClient::Request.execute(method: :get, url: url, verify_ssl: false, max_redirects: 0, headers: { Authorization: "Bearer #{token}" }).body
-      Nokogiri::XML(response)
-    end
-
-    def self.hash_to_uri(hash)
-      return hash.to_a.map { |x| "#{x[0]}=#{x[1]}" }.join("&")
-    end
-
-    def self.retryable(options = {}, &block)
-      opts = { :tries => 1, :on => Exception }.merge(options)
-
-      retry_exception, retries = opts[:on], opts[:tries]
-
-      begin
-        return yield
-      rescue retry_exception
-        retry if (retries -= 1) > 0
+      def request_xml(method, params)
+        url = build_url(method, params)
+        response = make_request(url)
+        Nokogiri::XML(response)
+      rescue Excon::Error => e
+        Rails.logger.error("[BoardGameGem] API request failed: #{e.message}")
+        raise BggApiError, "BGG API request failed: #{e.message}"
       end
 
-      yield
+      def build_url(method, params)
+        query_string = URI.encode_www_form(params)
+        "#{BGG_API_BASE}/#{method}?#{query_string}"
+      end
+
+      def make_request(url)
+        response = Excon.get(
+          url,
+          headers: default_headers,
+          read_timeout: REQUEST_TIMEOUT,
+          connect_timeout: REQUEST_TIMEOUT,
+          ssl_verify_peer: false
+        )
+
+        # BGG API returns 202 when data is being prepared, retry after delay
+        if response.status == 202
+          sleep 2
+          return make_request(url)
+        end
+
+        raise BggApiError, "BGG API returned status #{response.status}" unless response.status == 200
+
+        response.body
+      end
+
+      def default_headers
+        {
+          'User-Agent' => "Bordspellenvergelijken/1.0 (#{Rails.env})",
+          'Accept' => 'application/xml',
+          'Authorization' => "Bearer #{BGG_API_TOKEN}"
+        }
+      end
     end
   end
 end
